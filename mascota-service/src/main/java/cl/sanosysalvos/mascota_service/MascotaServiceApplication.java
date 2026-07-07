253a8432cbff88280f99fed9f90e4c3614e14e7a
JeanKaled <je.avilaa@duocuc.cl>
2026-07-07 10:50:46 -0400

    actualizacion

--- mascota-service/src/main/java/cl/sanosysalvos/mascota_service/MascotaServiceApplication.java
+++ mascota-service/src/main/java/cl/sanosysalvos/mascota_service/MascotaServiceApplication.java
@@ -2,7 +2,8 @@ package cl.sanosysalvos.mascota_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
+@ComponentScan(basePackages = "cl.sanosysalvos.mascota_service") // Asegúrate de que este sea el paquete raíz
 @SpringBootApplication
 public class MascotaServiceApplication {
 
